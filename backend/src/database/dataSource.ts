344ce2d9d1dfad3bcbc886870468b0ea59cbdbea
Bánó Balázs <banobalazs@BaluMac.local>
2022-10-09 16:00:50 +0200

    create datasource

---
+++ backend/src/database/dataSource.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm";
+import { DB_PORT, DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USERNAME } from "../configuration";
+
+export const AppDataSource = new DataSource({
+  migrationsTableName: "migrations",
+	type: "mysql",
+	host: DB_HOST,
+	port: Number(DB_PORT),
+	username: DB_USERNAME,
+	password: DB_PASSWORD,
+	database: DB_DATABASE,
+	synchronize: true,
+	logging: false,
+	multipleStatements: true,
+	entities: [],
+	subscribers: [],
+	migrations: [],
+})
