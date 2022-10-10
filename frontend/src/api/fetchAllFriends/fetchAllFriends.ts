78b913e1d30510e8661cbecdd79631c1ea673662
Bánó Balázs <banobalazs@BaluMac.local>
2022-10-10 11:03:30 +0200

    fetching all friends

--- frontend/src/api/fetchAllFriends/fetchAllFriends.ts
+++ frontend/src/api/fetchAllFriends/fetchAllFriends.ts
@@ -1,9 +1,29 @@
-import { request } from "../request";
+import { API_URL } from "../../configuration";
 
-export const fetchAllFriends = () =>
-	request<Object>({
-		endpoint: "friends/list",
-		config: {
-			method: "GET",
-		},
+// export const fetchAllFriends = () =>
+// 	request<Object>({
+// 		endpoint: "friends/list",
+// 		config: {
+// 			method: "GET",
+// 		},
+// 	});
+
+export const fetchAllFriends = async () => {
+	await fetch(`${API_URL}friends/list`)
+	.then((response) => {
+		if(!response.ok) {
+			throw new Error(
+				`This is an HTTP error: The status is ${response.status}`
+			);
+		}
+		return response.json()
+	})
+	.then((data) => {
+		console.log(data)
+		return data
+	})
+	.catch((err) => {
+		console.log(err.message);
 	});
+}
+
