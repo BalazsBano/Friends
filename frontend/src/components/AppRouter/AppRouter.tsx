2ad18c3d5488a4d0cfa958bbee54ba651bcc2e77
Bánó Balázs <banobalazs@BaluMac.local>
2022-10-11 21:26:18 +0200

    create routes to modify and new friend

--- frontend/src/components/AppRouter/AppRouter.tsx
+++ frontend/src/components/AppRouter/AppRouter.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { FriendsPage } from "../../pages/FriendsPage";
+import { FriendsPage, ModifyPage, NewFriendPage } from "../../pages";
 
 export function AppRouter() {
 	return (
 		<Routes>
-			<Route path="/" element={<FriendsPage />} />
+			<Route path="/modify" element={<ModifyPage />} />
+			<Route path="/new" element={<NewFriendPage />} />
+
+			<Route path="*" element={<FriendsPage />} />
 		</Routes>
 	);
 }
